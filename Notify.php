ce1484988938f080f86000cb1d050a6fc26e7cd4
Robert Boloc <robert.boloc@urv.cat>
2012-06-06 13:44:38 +0200

    added urgency parameter

--- Notify.php
+++ Notify.php
@@ -2,12 +2,16 @@
 
 namespace Notify;
 
+define('URGENCY_LOW', 'low');
+define('URGENCY_NORMAL', 'normal');
+define('URGENCY_CRITICAL', 'critical');
+
 class Notify {
 
-    const base = 'notify-send'; 
-
-    public static function show ($summary, $body) {
-        self::call(self::base.' '.$summary.' "'.$body.'"');          
+    const base = 'notify-send';
+    
+    public static function show ($summary, $body, $urgency = URGENCY_NORMAL) {
+        self::call(self::base.' '.$summary.' '.$body.' -u '.$urgency);          
     }
 
     private static function call ($cmd) {
